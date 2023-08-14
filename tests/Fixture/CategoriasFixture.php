ff66381cd830d0d3d2df6a4a467d6e77b5a191ef
Juan Carlos Vasquez Macias <carlosv97m@gmail.com>
2023-08-14 11:28:02 -0400

    first commit

---
+++ tests/Fixture/CategoriasFixture.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * CategoriasFixture
+ */
+class CategoriasFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'categoria' => 'Lorem ipsum dolor sit amet',
+                'estado' => 'Lorem ipsum dolor sit amet',
+            ],
+        ];
+        parent::init();
+    }
+}
